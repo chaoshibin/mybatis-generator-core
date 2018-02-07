f196ee42f5d6a2943e945875396cc2d1fbba1e59
chaoshibin <chaoshibin@jrq.com>
2018-02-07 13:42:33 +0800

    Mapper接口修改为Mapper

--- src/main/java/org/mybatis/generator/plugins/CustomCreateBeanPlugin.java
+++ src/main/java/org/mybatis/generator/plugins/CustomCreateBeanPlugin.java
@@ -1,17 +1,17 @@
 /**
- * Copyright 2006-2018 the original author or authors.
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *    Copyright 2006-2018 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
  */
 package org.mybatis.generator.plugins;
 
@@ -119,14 +119,14 @@ public class CustomCreateBeanPlugin extends PluginAdapter {
         abstractServiceType = new FullyQualifiedJavaType(servicePack + "." + "AbstractService");
         extendServiceInterfaceType = new FullyQualifiedJavaType(servicePack + "." + "Service<" + tableName + "," + tableName + "Example>");
         extendAbstractServiceType = new FullyQualifiedJavaType("AbstractService<" + tableName + "," + tableName + "Example>");
-        extendMapperType = new FullyQualifiedJavaType("IMapper<" + tableName + "," + tableName + "Example>");
+        extendMapperType = new FullyQualifiedJavaType("Mapper<" + tableName + "," + tableName + "Example>");
         // 【com.jrq.mapper.PetMapper】
         daoType = new FullyQualifiedJavaType(introspectedTable.getMyBatis3JavaMapperType());
 
         // 【com.jrq.service.impl.PetServiceImpl】logger.info(toLowerCase(daoType.getShortName()));
         serviceType = new FullyQualifiedJavaType(serviceImplPack + "." + tableName + "ServiceImpl");
 
-        mapperInterfaceType = new FullyQualifiedJavaType(mapperInterfacePack + ".IMapper");
+        mapperInterfaceType = new FullyQualifiedJavaType(mapperInterfacePack + ".Mapper");
 
         // 【com.jrq.domain.Pet】
         pojoType = new FullyQualifiedJavaType(pojoPack + "." + tableName);
